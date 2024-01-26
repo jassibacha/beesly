7077ba05b85b21aee7cac766074f78577c2acaf0
Jassi Bacha <klikster@gmail.com>
2024-01-26 12:23:24 -0800

    Page cleanup, starting dashboard

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -2,12 +2,13 @@
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
-      <div className="w-full flex-none md:w-64">
-        {/* <SideNav /> */}
-        Sidebar Nav
-      </div>
-      <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
-    </div>
+    <div className="">{children}</div>
+    // <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+    //   <div className="w-full flex-none md:w-64">
+    //     {/* <SideNav /> */}
+    //     Sidebar Nav
+    //   </div>
+    //   <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+    // </div>
   );
 }
