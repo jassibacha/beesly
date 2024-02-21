84ab1097f90dface496b74dd0aff9533f04c776e
Jassi Bacha <klikster@gmail.com>
2024-02-20 16:44:29 -0800

    Idea for modularized email sending

--- src/lib/sendgrid.ts
+++ src/lib/sendgrid.ts
@@ -52,3 +52,42 @@ export const sendEmail = async (
     console.error(error);
   }
 };
+
+// IDEA FOR MODULAR EMAIL TEMPLATE GENERATION
+
+// function xxx(bookingId / bookingObj, template) {
+//   // retrieve booking or pass it in as obj
+//   // generate dynamic data object based on booking (using the info in booking)
+
+//   // values = bookingObj
+//   //
+
+//   // switch or if/else statement to determine which template to use and then pull in the necessary booking, location, locationSettings and create the dynamicData that we need to send out.
+
+//   const dynamicData = {
+//     fromEmail: "book@jassibacha.com",
+//     fromName: "Book Test",
+//     replyEmail: "book@beesly.io",
+//     replyName: "Beesly",
+//     subject: `Booking Confirmation - ${DateTime.fromJSDate(values.date).toFormat("DDDD")}`,
+//     preheader: `${DateTime.fromJSDate(values.date).toFormat("DDDD")} at ${DateTime.fromISO(values.timeSlot).toFormat("h:mm a")} confirmed!`,
+//     heading: "Booking Confirmed!",
+//     textBody: `Dear ${values.customerName}, your booking for ${DateTime.fromJSDate(values.date).toFormat("DDDD")} at ${DateTime.fromISO(values.timeSlot).toFormat("h:mm a")} has been confirmed.`,
+//     date: DateTime.fromJSDate(values.date).toFormat("DDDD"),
+//     startTime: DateTime.fromISO(values.timeSlot)
+//       .setZone(locationSettings.timeZone)
+//       .toFormat("h:mm a"),
+//     endTime: DateTime.fromISO(values.timeSlot)
+//       .plus({ minutes: parseFloat(values.duration) * 60 })
+//       .setZone(locationSettings.timeZone)
+//       .toFormat("h:mm a"),
+//     customerName: values.customerName,
+//     customerEmail: values.customerEmail,
+//     customerPhone: values.customerPhone,
+//     locationName: location.name,
+//     locationPhone: location.phone,
+//     locationEmail: location.email,
+//     locationLogo: location.logo,
+//   },
+
+// }
