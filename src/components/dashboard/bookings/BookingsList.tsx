5823259a6e57ae8c98c0b4e42a403c342938f5ae
Jassi Bacha <klikster@gmail.com>
2024-03-12 17:13:19 -0700

    Proper loading state for BookingsList

--- src/components/dashboard/bookings/BookingsList.tsx
+++ src/components/dashboard/bookings/BookingsList.tsx
@@ -15,6 +15,7 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { Loader2 } from "lucide-react";
 /* eslint-disable @typescript-eslint/no-empty-function, @typescript-eslint/no-misused-promises */
 
 interface BookingsListProps {
@@ -37,30 +38,75 @@ export default function BookingsList({
 }: BookingsListProps) {
   let bookings: Booking[] = [];
   let refetch: () => void = () => {}; // eslint-disable-line @typescript-eslint/no-empty-function
+  let isLoading = false;
   let title = "";
   let desc = "";
 
   if (type === "upcoming") {
-    const { data: bookingsData, refetch: refetchUpcoming } =
-      api.booking.listUpcomingBookings.useQuery({
-        locationId: location.id,
-        limit: limit,
-      });
+    const {
+      data: bookingsData,
+      refetch: refetchUpcoming,
+      isLoading: isLoadingUpcoming,
+    } = api.booking.listUpcomingBookings.useQuery({
+      locationId: location.id,
+      limit: limit,
+    });
     bookings = bookingsData?.bookings ?? [];
     refetch = refetchUpcoming;
+    isLoading = isLoadingUpcoming;
     title = "Upcoming Bookings";
     desc = "There are X bookings today"; // TODO: Make this dynamic (additional query inside of trpc?)
   } else if (type === "recent") {
-    const { data: bookingsData, refetch: refetchRecent } =
-      api.booking.listRecentBookings.useQuery({
-        locationId: location.id,
-        limit: limit,
-      });
+    const {
+      data: bookingsData,
+      refetch: refetchRecent,
+      isLoading: isLoadingRecent,
+    } = api.booking.listRecentBookings.useQuery({
+      locationId: location.id,
+      limit: limit,
+    });
     bookings = bookingsData?.bookings ?? [];
     refetch = refetchRecent;
+    isLoading = isLoadingRecent;
     title = "Recent Bookings";
     desc = "You have X bookings in the past 24h"; // TODO: Make this dynamic (additional query inside of trpc?)
   }
+
+  const renderBookings = () => {
+    if (isLoading) {
+      return (
+        <div className="flex items-center justify-center space-x-2">
+          <Loader2 className="animate-spin" />{" "}
+          {/* Loading icon with spin animation */}
+          <span>Loading...</span>
+        </div>
+      );
+    }
+
+    if (bookings.length === 0) {
+      return (
+        <div className="flex items-center justify-center space-x-2">
+          <div>No bookings found</div>
+        </div>
+      );
+    }
+
+    return (
+      <div className="space-y-8">
+        {bookings.map((booking) => (
+          <BookingItem
+            key={booking.id}
+            booking={booking}
+            timezone={timezone}
+            location={location}
+            locationSettings={locationSettings}
+            resources={resources}
+            refetch={refetch}
+          />
+        ))}
+      </div>
+    );
+  };
 
   return (
     <Card className="">
@@ -68,21 +114,7 @@ export default function BookingsList({
         <CardTitle>{title}</CardTitle>
         <CardDescription>{desc}</CardDescription>
       </CardHeader>
-      <CardContent>
-        <div className="space-y-8">
-          {bookings.map((booking) => (
-            <BookingItem
-              key={booking.id}
-              booking={booking}
-              timezone={timezone}
-              location={location}
-              locationSettings={locationSettings}
-              resources={resources}
-              refetch={refetch}
-            />
-          ))}
-        </div>
-      </CardContent>
+      <CardContent>{renderBookings()}</CardContent>
     </Card>
   );
 }
