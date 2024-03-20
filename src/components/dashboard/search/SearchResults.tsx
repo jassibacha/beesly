0ab0ab0f2179f4497026cf6b3e30b36aa1b44734
Jassi Bacha <klikster@gmail.com>
2024-03-20 15:30:32 -0700

    Cleanup code before starting data table

--- src/components/dashboard/search/SearchResults.tsx
+++ src/components/dashboard/search/SearchResults.tsx
@@ -25,25 +25,11 @@ export default async function SearchResults({
   currentPage: number;
   location: Location;
 }) {
-  //const invoices = await fetchFilteredInvoices(query, currentPage);
-
-  //
+  // We could do this as a client fetch instead
   const bookingResults = await api.booking.searchBookings.query({
     query: query,
     locationId: location.id,
   });
-  // const [searchQuery, setSearchQuery] = useState("");
-  // const [currentPage, setCurrentPage] = useState(1);
-  // const totalPages = 10; // This should be dynamically calculated based on your data
-
-  // const handleSearch = (query: string) => {
-  //   setSearchQuery(query);
-  //   setCurrentPage(1); // Reset to the first page when a new search is performed
-  // };
-
-  // const handlePageChange = (page: number) => {
-  //   setCurrentPage(page);
-  // };
 
   return (
     <>
@@ -100,65 +86,6 @@ export default async function SearchResults({
           ))}
         </TableBody>
       </Table>
-      {/* <div>
-        {bookingResults?.map((booking) => (
-          <div key={booking.id}>
-            <h2>{booking.customerName}</h2>
-            <p>
-              {booking.customerEmail} {booking.customerPhone}
-            </p>
-          </div>
-        ))}
-      </div> */}
     </>
   );
-  {
-    /* <SearchInput onSearch={handleSearch} /> */
-  }
-  {
-    /* {searchQuery} */
-  }
-  {
-    /* Render your search results here */
-  }
-  {
-    /* <Pagination
-        currentPage={currentPage}
-        totalPages={totalPages}
-        onPageChange={handlePageChange}
-      /> */
-  }
 }
-
-// async function fetchFilteredInvoices(query: string, currentPage: number) {
-//   noStore();
-//   const offset = (currentPage - 1) * ITEMS_PER_PAGE;
-
-//   try {
-//     const invoices = await sql<InvoicesTable>`
-//       SELECT
-//         invoices.id,
-//         invoices.amount,
-//         invoices.date,
-//         invoices.status,
-//         customers.name,
-//         customers.email,
-//         customers.image_url
-//       FROM invoices
-//       JOIN customers ON invoices.customer_id = customers.id
-//       WHERE
-//         customers.name ILIKE ${`%${query}%`} OR
-//         customers.email ILIKE ${`%${query}%`} OR
-//         invoices.amount::text ILIKE ${`%${query}%`} OR
-//         invoices.date::text ILIKE ${`%${query}%`} OR
-//         invoices.status ILIKE ${`%${query}%`}
-//       ORDER BY invoices.date DESC
-//       LIMIT ${ITEMS_PER_PAGE} OFFSET ${offset}
-//     `;
-
-//     return invoices.rows;
-//   } catch (error) {
-//     console.error("Database Error:", error);
-//     throw new Error("Failed to fetch invoices.");
-//   }
-// }
