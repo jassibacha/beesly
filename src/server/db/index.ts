52153a3c0736a04f8f3e6eb27fb7347d14747b44
Jassi Bacha <klikster@gmail.com>
2024-04-03 13:23:16 -0700

    Testing neon serverless, issue with middleware on dashboard

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,5 +1,5 @@
 import * as schema from "./schema";
-import { sql } from "@vercel/postgres";
+import { sql as VercelSQL } from "@vercel/postgres";
 import postgres from "postgres";
 import {
   drizzle as VercelDrizzle,
@@ -10,6 +10,15 @@ import {
   drizzle as LocalDrizzle,
   type PostgresJsDatabase,
 } from "drizzle-orm/postgres-js";
+import { neon, Pool } from "@neondatabase/serverless";
+// import {
+//   drizzle as NeonDrizzle,
+//   type NeonHttpDatabase as NeonDatabase,
+// } from "drizzle-orm/neon-http";
+import {
+  drizzle as NeonDrizzle,
+  type NeonDatabase,
+} from "drizzle-orm/neon-serverless";
 import { env } from "@/env";
 
 // https://www.thisdot.co/blog/configure-your-project-with-drizzle-for-local-and-deployed-databases
@@ -24,26 +33,75 @@ type MySchema = typeof schema;
 
 // Define a variable 'db' that can be either a VercelPgDatabase or a PostgresJsDatabase,
 // both typed with the schema structure.
-let db: VercelPgDatabase<MySchema> | PostgresJsDatabase<MySchema>;
+let db: VercelPgDatabase<MySchema> | NeonDatabase<MySchema>;
 
-// Check the environment to determine which database to use.
-if (
-  process.env.NODE_ENV === "production" ||
-  process.env.NODE_ENV === "development"
-) {
-  // In production or development, use Vercel Postgres with the defined schema.
-  db = VercelDrizzle(sql as VercelPgClient, { schema });
-  //db = VercelDrizzle(sql);
-  //console.log("Using Vercel Postgres (supabase)");
+// Determine which database URL to use based on the environment
+//const databaseUrl = process.env.APP_ENV === "test" ? env.TEST_DATABASE_URL : env.DATABASE_URL;
+
+console.log("NODE_ENV: ", process.env.NODE_ENV);
+// This shows that playwright tests are coming through as NODE_ENV=development
+console.log("APP_ENV: ", process.env.APP_ENV);
+//console.log("ENVIRONMENT: ", process.env.ENVIRONMENT);
+
+// VERSION 3
+// Check custom APP_ENV variable to determine which database to use
+if (process.env.APP_ENV === "test") {
+  // In the test environment, use neon database. TEST_DATABASE_URL is connectionString
+  // Http
+  // const sql = neon(process.env.TEST_DATABASE_URL!);
+  // db = NeonDrizzle(sql, { schema });
+
+  // Serverless
+  const pool = new Pool({ connectionString: process.env.TEST_DATABASE_URL });
+  db = NeonDrizzle(pool);
+  console.log("Using Neon Postgres for Testing");
 } else {
-  // In the test environment, use a local Postgres database.
-  // The TEST_DATABASE_URL environment variable should contain the connection string.
-  const migrationClient = postgres(process.env.TEST_DATABASE_URL!); // removed 'as string' from end
-  // Initialize the database with the local Postgres client and the defined schema.
-  db = LocalDrizzle(migrationClient, { schema });
-  //db = LocalDrizzle(migrationClient);
-  //console.log("Using Local Postgres for Testing");
+  // In production or development, use Vercel Postgres with the defined schema.
+  db = VercelDrizzle(VercelSQL as VercelPgClient, { schema });
+  console.log("Using Vercel Postgres (supabase)");
 }
+
+// // Define a variable 'db' that can be either a VercelPgDatabase or a PostgresJsDatabase,
+// // both typed with the schema structure.
+// let db: VercelPgDatabase<MySchema> | PostgresJsDatabase<MySchema>;
+
+// VERSION 2
+// Check custom APP_ENV variable to determine which database to use
+// This can be refactored later to separate production and development if necessary
+// if (process.env.APP_ENV === "test") {
+//   // In the test environment, use a local Postgres database.
+//   // The TEST_DATABASE_URL environment variable should contain the connection string.
+//   const migrationClient = postgres(process.env.TEST_DATABASE_URL!); // removed 'as string' from end
+//   // Initialize the database with the local Postgres client and the defined schema.
+//   db = LocalDrizzle(migrationClient, { schema });
+//   //db = LocalDrizzle(migrationClient);
+//   console.log("Using Local Postgres for Testing");
+// } else {
+//   // In production or development, use Vercel Postgres with the defined schema.
+//   db = VercelDrizzle(sql as VercelPgClient, { schema });
+//   //db = VercelDrizzle(sql);
+//   console.log("Using Vercel Postgres (supabase)");
+// }
+
+// VERSION 1
+// // Check the environment to determine which database to use.
+// if (
+//   process.env.NODE_ENV === "production" ||
+//   process.env.NODE_ENV === "development"
+// ) {
+//   // In production or development, use Vercel Postgres with the defined schema.
+//   db = VercelDrizzle(sql as VercelPgClient, { schema });
+//   //db = VercelDrizzle(sql);
+//   //console.log("Using Vercel Postgres (supabase)");
+// } else {
+//   // In the test environment, use a local Postgres database.
+//   // The TEST_DATABASE_URL environment variable should contain the connection string.
+//   const migrationClient = postgres(process.env.TEST_DATABASE_URL!); // removed 'as string' from end
+//   // Initialize the database with the local Postgres client and the defined schema.
+//   db = LocalDrizzle(migrationClient, { schema });
+//   //db = LocalDrizzle(migrationClient);
+//   //console.log("Using Local Postgres for Testing");
+// }
 
 export { db };
 
--- src/server/db/migrate.mts
+++ src/server/db/migrate.mts
@@ -1,34 +1,72 @@
 import "dotenv/config";
+import * as schema from "./schema";
+import { neon, Pool } from "@neondatabase/serverless";
 import { migrate as VercelMigrate } from "drizzle-orm/vercel-postgres/migrator";
-import { migrate as LocalMigrate } from "drizzle-orm/postgres-js/migrator";
-import postgres from "postgres";
-import { sql } from "@vercel/postgres";
+//import { migrate as NeonMigrate } from "drizzle-orm/neon-http/migrator";
+import { migrate as NeonMigrate } from "drizzle-orm/neon-serverless/migrator";
+//import { migrate as LocalMigrate } from "drizzle-orm/postgres-js/migrator";
+//import postgres from "postgres";
+import { sql as VercelSQL } from "@vercel/postgres";
 import {
   drizzle as VercelDrizzle,
-  VercelPgDatabase,
+  type VercelPgDatabase,
 } from "drizzle-orm/vercel-postgres";
+// import {
+//   drizzle as NeonDrizzle,
+//   type NeonHttpDatabase as NeonDatabase,
+// } from "drizzle-orm/neon-http";
 import {
-  drizzle as LocalDrizzle,
-  PostgresJsDatabase,
-} from "drizzle-orm/postgres-js";
+  drizzle as NeonDrizzle,
+  type NeonDatabase,
+} from "drizzle-orm/neon-serverless";
+// import {
+//   drizzle as LocalDrizzle,
+//   PostgresJsDatabase,
+// } from "drizzle-orm/postgres-js";
 
 // https://www.thisdot.co/blog/configure-your-project-with-drizzle-for-local-and-deployed-databases#creating-database-tables
 
 // Could not import from drizzle.ts (index.ts) due to mts v ts compatibility issues
 // NOTE: If I change to .ts file could I avoid part of this code?
-let db;
+//let db;
 
-if (
-  process.env.NODE_ENV === "production" ||
-  process.env.NODE_ENV === "development"
-) {
-  db = VercelDrizzle(sql);
-  await VercelMigrate(db, { migrationsFolder: "./db/migrations" });
+// Define a type that represents the structure of the database schema.
+type MySchema = typeof schema;
+
+// Define a variable 'db' that can be either a VercelPgDatabase or a NeonDatabase,
+// both typed with the schema structure.
+let db: VercelPgDatabase<MySchema> | NeonDatabase<MySchema>;
+
+if (process.env.APP_ENV === "test") {
+  // Connect this to test DB, not sure if we want serverless or http here if either is better for migration
+  // Http
+  // const sql = neon(process.env.TEST_DATABASE_URL!);
+  // db = NeonDrizzle(sql, { schema });
+
+  // Serverless
+  const pool = new Pool({ connectionString: process.env.TEST_DATABASE_URL });
+  db = NeonDrizzle(pool);
+  await NeonMigrate(db, { migrationsFolder: "./db/migrations" });
 } else {
-  const migrationClient = postgres(process.env.TEST_DATABASE_URL!, {
-    max: 1,
-  });
-  db = LocalDrizzle(migrationClient);
-  await LocalMigrate(db, { migrationsFolder: "./db/migrations" });
-  await migrationClient.end();
+  // Main dev/prod DB
+  db = VercelDrizzle(VercelSQL, { schema });
+  await VercelMigrate(db, { migrationsFolder: "./db/migrations" });
 }
+
+// Original config with local db
+// let db;
+
+// if (
+//   process.env.NODE_ENV === "production" ||
+//   process.env.NODE_ENV === "development"
+// ) {
+//   db = VercelDrizzle(sql);
+//   await VercelMigrate(db, { migrationsFolder: "./db/migrations" });
+// } else {
+//   const migrationClient = postgres(process.env.TEST_DATABASE_URL!, {
+//     max: 1,
+//   });
+//   db = LocalDrizzle(migrationClient);
+//   await LocalMigrate(db, { migrationsFolder: "./db/migrations" });
+//   await migrationClient.end();
+// }
