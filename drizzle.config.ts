5ad75c5b94987a815043fe7301296e337ee1629d
Jassi Bacha <klikster@gmail.com>
2024-04-03 14:40:42 -0700

    Setup APP_ENV=test inside of drizzle.config.ts for db:studio-test script

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,12 +2,19 @@ import { type Config } from "drizzle-kit";
 
 import { env } from "@/env";
 
+// if process.env.APP_ENV === "test" then make const dbString = process.env.TEST_DATABASE_URL otherwise use process.env.DATABASE_URL
+const dbString =
+  process.env.APP_ENV === "test"
+    ? process.env.TEST_DATABASE_URL
+    : process.env.DATABASE_URL;
+
 export default {
   schema: "./src/server/db/schema.ts",
   out: "./db/migrations",
   driver: "pg",
   dbCredentials: {
-    connectionString: env.DATABASE_URL,
+    connectionString: dbString!,
+    //connectionString: env.DATABASE_URL,
     // This connectionString doesn't affect the actual DB for the app
     // But changing it DID affect Drizzle Studio
   },
